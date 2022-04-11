b61ea1f692bddac1a0989221456f3f81436a1ba3
Aiman Aziz <muhdaimanaziz93@gmail.com>
2022-04-12 00:14:40 +0800

    add static url

--- core/urls.py
+++ core/urls.py
@@ -35,6 +35,6 @@ urlpatterns = [
         description="",
         version="1.0.1",
     ), name='schema')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
