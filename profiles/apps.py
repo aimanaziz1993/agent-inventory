a4eaabdc0e696b6dafea5af55bc081be73343d72
Aiman Aziz <muhdaimanaziz93@gmail.com>
2022-04-12 20:42:33 +0800

    remove import apps

--- profiles/apps.py
+++ profiles/apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 from django.db.models.signals import post_save
 
 from users.models import NewUser
-from profiles.signals import create_user_profile, save_user_profile
 
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
