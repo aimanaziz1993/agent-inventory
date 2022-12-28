40a390ab81dbfd1f0d548d63e7426b934fd65767
hamiruldev <hamirul.dev@gmail.com>
2022-12-28 08:33:04 +0800

    add category

--- inventory_api/filters.py
+++ inventory_api/filters.py
@@ -8,7 +8,8 @@ class CustomInventoryFilter(filters.FilterSet):
     title = filters.CharFilter(field_name='title', lookup_expr='icontains')
     location = filters.CharFilter(field_name='location', lookup_expr='icontains')
     username = filters.CharFilter(field_name='realtor__user__user_name', lookup_expr='icontains')
+    category = filters.CharFilter(field_name='category', lookup_expr='exact')
 
     class Meta:
         model = Inventory
-        fields = ['username', 'title', 'location']
+        fields = ['username', 'title', 'location', 'category']
